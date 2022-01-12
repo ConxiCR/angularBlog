90a1c7cd97c3c3d67e9786f150d73364780e1650
ConxiCr <conxicr@hotmail.com>
2022-01-12 16:22:35 +0100

    Library installation angular material and module
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

---
+++ src/app/material.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+const myModule = [
+  MatCardModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, myModule],
+  exports: [myModule]
+})
+export class MaterialModule { }
