de430e20a0b340212a00478ec9e9aafa2e8c276b
ConxiCr <conxicr@hotmail.com>
2022-01-12 15:37:10 +0100

    creation of componets and routes
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'pots', loadChildren: () => import('./components/posts/list-posts/list-posts.module').then(m => m.ListPostsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
